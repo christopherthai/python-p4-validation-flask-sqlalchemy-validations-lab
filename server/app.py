e83c7c0cdc88c8de14011b310c819a8a182bfa94
Christopher Thai <christopherthai.nyli@gmail.com>
2024-04-23 17:20:05 -0400

    Complete Lab

--- server/app.py
+++ server/app.py
@@ -4,16 +4,18 @@ from flask_migrate import Migrate
 from models import db, Author, Post
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
-def index():
-    return 'Validations lab'
 
-if __name__ == '__main__':
+@app.route("/")
+def index():
+    return "Validations lab"
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
